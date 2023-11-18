db825e242460da939d2f996714d1913c369e0ec7
dmitriyalushkin <pantific37@mail.ru>
2023-11-18 22:51:06 +0300

    Initial commit

---
+++ project/catalog/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def contacts(request):
+    return render(request, 'main/contacts.html')
+
+
+def home(request):
+    return render(request, 'main/home.html')
