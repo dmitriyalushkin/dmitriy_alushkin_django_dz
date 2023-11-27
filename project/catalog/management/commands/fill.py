96f52491c5345b9d8767f1c5a4a57c9448f76075
dmitriyalushkin <pantific37@mail.ru>
2023-11-27 09:21:22 +0300

    Initial commit 5

--- project/catalog/management/commands/fill.py
+++ project/catalog/management/commands/fill.py
@@ -1,34 +1,23 @@
 from django.core.management import BaseCommand
 
+
 from catalog.models import Product, Category
 
 
 class Command(BaseCommand):
+    requires_migrations_checks = True
 
-    def handle(self, *args, **options):
-        Category.objects.all().delete()
-        Product.objects.all().delete()
-        category_list = [
-            {'name': 'пряник', 'description': 'вкусный'},
-            {'name': 'сок', 'description': 'яблочный'}
-        ]
-
-        category_for_create = []
-        for category_item in category_list:
-            category_for_create.append(Category(**category_item))
-
-        Category.objects.bulk_create(category_for_create)
-
-
-
-        product_list = [
-            {'name': 'тульский', 'price': 50, 'category': 25},
-            {'name': 'добрый', 'price': 100, 'category': 26}
-        ]
-
-        product_for_create = []
-        for product_item in product_list:
-            product_for_create.append(Product(**product_item))
-
-        Product.objects.bulk_create(product_for_create)
+    def handle(self, *args, **options) -> None:
+        fixtures_path = 'project/catalog_data.json'  # Тут укажи путь до фикстур
+        try:
+            call_command('loaddata', fixtures_path)
+        except ProgrammingError:
+            pass
+        except IntegrityError as e:
+            self.stdout.write(f'Invalid fixtures: {e}', self.style.NOTICE)
+        else:
+            self.stdout.write(
+                'Command  have been completed successfully',
+                self.style.SUCCESS
+            )
 
