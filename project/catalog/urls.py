6575b346480b4454858f11f4197d3985b29395d3
dmitriyalushkin <pantific37@mail.ru>
2023-12-04 19:30:28 +0300

    Initial commit add 2

--- project/catalog/urls.py
+++ project/catalog/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from catalog.views import contacts, index, product_detail, categories
 from catalog.views import index
 
-# from catalog.views import CategoryListView, ProductListView
-# from catalog.views import BlogEntryDetailView, BlogEntryListView, BlogEntryCreateView, BlogEntryUpdateView
+from catalog.views import CategoryListView, ProductListView
+from catalog.views import BlogEntryDetailView, BlogEntryListView, BlogEntryCreateView, BlogEntryUpdateView
 
 from catalog.apps import CatalogConfig
 
@@ -15,10 +15,10 @@ urlpatterns = [
     path('contacts/', contacts, name='contacts'),
     path('category_list/', categories, name='categories-list'),
     path('product/<int:pk>/', product_detail, name='product-detail'),
-    # path('category_list/', CategoryListView.as_view(), name='categories-list'),
-    # path('product/<int:pk>/', ProductListView.as_view(), name='product-detail'),
-    # path('view/<int:pk>/', BlogEntryDetailView.as_view(), name='blog-entry-detail'),
-    # path('list/', BlogEntryListView.as_view(), name='blog-entry-list'),
-    # path('create/', BlogEntryCreateView.as_view(), name='blog-entry-create'),
-    # path('edit/<int:pk>/', BlogEntryUpdateView.as_view(), name='blog-entry-edit')
+    path('category_list/', CategoryListView.as_view(), name='categories-list'),
+    path('product/<int:pk>/', ProductListView.as_view(), name='product-detail'),
+    path('view/<int:pk>/', BlogEntryDetailView.as_view(), name='blog-entry-detail'),
+    path('list/', BlogEntryListView.as_view(), name='blog-entry-list'),
+    path('create/', BlogEntryCreateView.as_view(), name='blog-entry-create'),
+    path('edit/<int:pk>/', BlogEntryUpdateView.as_view(), name='blog-entry-edit')
 ]
--- project/catalog/views.py
+++ project/catalog/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render, get_object_or_404
 
 from catalog.models import Category, Product, BlogEntry
 
-# from django.views.generic import ListView, DetailView, CreateView, UpdateView
-#
-# from django.urls import reverse_lazy, reverse
-#
-# from pytils.translit import slugify
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+
+from django.urls import reverse_lazy, reverse
+
+from pytils.translit import slugify
 
 # Create your views here.
 
@@ -55,75 +55,75 @@ def product_detail(request, pk):
 #     return render(request, 'main/product-detail.html', context)
 
 
-# class CategoryListView(ListView):
-#     model = Category
-#     extra_context = {
-#         'title': 'Продукты'
-#     }
-#
-#
-# class ProductListView(ListView):
-#     model = Product
-#
-#
-#     def get_context_data(self, *args, **kwargs):
-#         context_data = super().get_context_data(*args, **kwargs)
-#         context_data['object'] = get_object_or_404(Product, pk=self.kwargs.get('pk'))
-#         context_data['title'] = Product.category
-#
-#         return context_data
-#
-#
-# class BlogEntryDetailView(DetailView):
-#     model = BlogEntry
-#
-#     def get_object(self, queryset=None):
-#         self.object = super().get_object(queryset)
-#         self.object.views_count += 1
-#         self.object.save()
-#         return self.object
-#
-#
-# class BlogEntryListView(ListView):
-#     model = BlogEntry
-#
-#     def get_queryset(self, *args, **kwargs):
-#         queryset = super().get_queryset(*args, **kwargs)
-#         queryset = queryset.filter(sign_of_publication=True)
-#         return queryset
-#
-#
-# class BlogEntryCreateView(CreateView):
-#     model = BlogEntry
-#     fields = ('header', 'content',)
-#     success_url = reverse_lazy('main:blog-entry-list')
-#     template_name = 'main/blogentry_form.html'
-#
-#     def form_valid(self, form):
-#         if form.is_valid():
-#             new_blog_entry = form.save()
-#             new_blog_entry.slug = slugify(new_blog_entry.title)
-#             new_blog_entry.save()
-#
-#         return super().form_valid(form)
-#
-#
-# class BlogEntryUpdateView(UpdateView):
-#     model = BlogEntry
-#     fields = ('header', 'content',)
-#     # success_url = reverse_lazy('main:blog-entry-list')
-#
-#     def form_valid(self, form):
-#         if form.is_valid():
-#             new_blog_entry = form.save()
-#             new_blog_entry.slug = slugify(new_blog_entry.title)
-#             new_blog_entry.save()
-#
-#         return super().form_valid(form)
-#
-#
-#     def get_success_url(self):
-#         return reverse('main:view', args=[self.kwargs.get('pk')])
+class CategoryListView(ListView):
+    model = Category
+    extra_context = {
+        'title': 'Продукты'
+    }
+
+
+class ProductListView(ListView):
+    model = Product
+
+
+    def get_context_data(self, *args, **kwargs):
+        context_data = super().get_context_data(*args, **kwargs)
+        context_data['object'] = Product.objects.get(pk=self.kwargs.get('pk'))
+        context_data['title'] = Product.name
+
+        return context_data
+
+
+class BlogEntryDetailView(DetailView):
+    model = BlogEntry
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.views_count += 1
+        self.object.save()
+        return self.object
+
+
+class BlogEntryListView(ListView):
+    model = BlogEntry
+
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset(*args, **kwargs)
+        queryset = queryset.filter(sign_of_publication=True)
+        return queryset
+
+
+class BlogEntryCreateView(CreateView):
+    model = BlogEntry
+    fields = ('header', 'content',)
+    success_url = reverse_lazy('main:blog-entry-list')
+    template_name = 'main/blogentry_form.html'
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_blog_entry = form.save()
+            new_blog_entry.slug = slugify(new_blog_entry.header)
+            new_blog_entry.save()
+
+        return super().form_valid(form)
+
+
+class BlogEntryUpdateView(UpdateView):
+    model = BlogEntry
+    fields = ('header', 'content',)
+    # success_url = reverse_lazy('main:blog-entry-list')
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_blog_entry = form.save()
+            new_blog_entry.slug = slugify(new_blog_entry.header)
+            new_blog_entry.save()
+
+        return super().form_valid(form)
+
+
+    def get_success_url(self):
+        return reverse('main:view', args=[self.kwargs.get('pk')])
 
 
 
